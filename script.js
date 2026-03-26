df160a994416f44ec947b8ca3304ade5a124b9d3
Ruth Damayanthy Purba <127615528+ruthdamayy@users.noreply.github.com>
2026-03-26 16:28:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.querySelectorAll("a[href^='#']").forEach(anchor => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+
+// Reveal animation
+const elements = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  elements.forEach(el => {
+    const pos = el.getBoundingClientRect().top;
+    if (pos < window.innerHeight - 100) {
+      el.style.opacity = 1;
+      el.style.transform = "translateY(0)";
+    }
+  });
+});
